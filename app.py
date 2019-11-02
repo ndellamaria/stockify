1c834f4b2f8e9999923d3cfcc12472990a0c52d0
ndellamaria <nrdell3@gmail.com>
2019-11-02 00:33:33 -0400

    added about page with name form

--- app.py
+++ app.py
@@ -1,8 +1,17 @@
-from flask import Flask
+from flask import Flask, render_template, request
 app = Flask(__name__)
 
+# Homepage
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+# About
+@app.route('/about', methods=['GET','POST'])
+def about():
+	if request.method == 'POST':
+		return render_template('about.html', name=request.form['name'])
+	else:
+		return render_template('about.html')
 
 app.run()
